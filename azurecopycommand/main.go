c971390b0c63d2deb4660cf4cc47e623c66b1143
Ken Faulkner <ken.faulkner@gmail.com>
2016-09-29 18:19:27 +1000

    moving fmt.println to log

--- azurecopycommand/main.go
+++ azurecopycommand/main.go
@@ -3,7 +3,6 @@ package main
 import (
 	"azurecopy/azurecopy"
 	"azurecopy/azurecopy/models"
-	"fmt"
 )
 
 // "so it begins"
@@ -13,16 +12,10 @@ func main() {
 
 	rootContainer := ac.GetRootContainer(models.Azure)
 
-	fmt.Println(rootContainer.ContainerSlice[0].Name)
-
 	for _, c := range rootContainer.ContainerSlice {
 		if c.Name == "temp" {
 			ac.GetContainerContents(c)
-
 			c.DisplayContainer("")
-
-			fmt.Println("Container children ", len(c.ContainerSlice))
-			fmt.Println("Container2  children ", len(c.ContainerSlice[0].ContainerSlice))
 		}
 	}
 
